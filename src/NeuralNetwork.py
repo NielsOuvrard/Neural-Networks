a41b66864e25accf8e6dafbfe458b6be7bc5d1e2
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2023-12-09 22:01:23 +0100

    🚧

---
+++ src/NeuralNetwork.py
@@ -0,0 +1,67 @@
+import numpy as np
+import json
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+class NeuralNetwork:
+    def __init__(self, layers_sizes):
+        self.layers_sizes = layers_sizes
+        self.num_layers = len(layers_sizes)
+        self.weights = [np.random.uniform(-1, 1, (layers_sizes[i+1], layers_sizes[i])) for i in range(self.num_layers - 1)]
+        self.biases = [np.zeros((size, 1)) for size in layers_sizes[1:]]
+        self.activations = [np.zeros((size, 1)) for size in layers_sizes]
+        self.learning_rate = 0.01
+
+    def feed_forward(self, inputs):
+        self.activations[0] = inputs.reshape((len(inputs), 1))
+
+        for i in range(self.num_layers - 1):
+            weighted_sum = np.dot(self.weights[i], self.activations[i]) + self.biases[i]
+            self.activations[i + 1] = sigmoid(weighted_sum)
+
+        return self.activations[-1]
+
+    def train(self, inputs, expected_output):
+        # Perform feedforward to compute activations
+        self.feed_forward(inputs)
+
+        # Compute the error at the output layer
+        output_error = expected_output - self.activations[-1]
+
+        # Backpropagation
+        for i in reversed(range(self.num_layers - 1)):
+            # Compute the gradient at the current layer
+            gradient = sigmoid_derivative(self.activations[i + 1]) * output_error
+            gradient *= self.learning_rate
+
+            # Update weights and biases
+            self.weights[i] += np.dot(gradient, self.activations[i].T)
+            self.biases[i] += gradient
+
+            # Propagate the error to the previous layer
+            output_error = np.dot(self.weights[i].T, output_error)
+
+    def cost(self, inputs, expected_output):
+        predictions = self.feed_forward(inputs)
+        return np.sum((expected_output - predictions) ** 2)
+
+    def predict(self, inputs):
+        return self.feed_forward(inputs)
+
+    def save(self, filename):
+        data = {
+            'weights': [arr.tolist() for arr in self.weights],
+            'biases': [arr.tolist() for arr in self.biases]
+        }
+        with open(filename, 'w') as file:
+            json.dump(data, file)
+
+    def load(self, filename):
+        with open(filename, 'r') as file:
+            data = json.load(file)
+        self.weights = [np.array(data_list) for data_list in data['weights']]
+        self.biases = [np.array(data_list) for data_list in data['biases']]
--- src/file.py
+++ src/file.py
@@ -1,5 +1,53 @@
 import sys
-from arguments import Arguments, handle_arguments, print_usage
+import pandas as pd
+import numpy as np
+from arguments import Arguments, handle_arguments
+from NeuralNetwork import NeuralNetwork
+
+def execute(args : Arguments):
+    if args.new_perceptron:
+        perceptron_data = pd.read_csv(args.input_file)
+        to_train = perceptron_data.values.tolist()
+        # Set up the NeuralNetwork
+        input_size = args.nb_inputs
+        output_size = 1
+        hidden_layer_sizes = [8]  # adjust the number of hidden layers and nodes
+
+        nn = NeuralNetwork([input_size] + hidden_layer_sizes + [output_size])
+
+        # Training loop
+        epochs = 10000
+        for _ in range(epochs):
+            for data_point in to_train:
+                inputs = np.array(data_point[:-1])
+                expected_output = np.array([data_point[-1]])
+                nn.train(inputs, expected_output)
+
+        # Test the trained model on AND gate inputs
+        for data_point in to_train:
+            inputs = np.array(data_point[:-1])
+            expected_output = np.array([data_point[-1]])
+            prediction = nn.predict(inputs)
+            print(f"Inputs: {inputs}, Expected: {expected_output}, Predicted: {prediction} = {round(prediction[0][0])}")
+
+        if args.save_perceptron:
+            nn.save(args.save_file)
+
+    elif args.load_perceptron:
+        nn = NeuralNetwork([2, 8, 1])
+        nn.load(args.load_file)
+        perceptron_data = pd.read_csv(args.input_file)
+        # line_of_data = len(perceptron_data.columns) - 1
+        to_predict = perceptron_data.values.tolist()
+        for data_point in to_predict:
+            inputs = np.array(data_point[:-1])
+            expected_output = np.array([data_point[-1]])
+            prediction = nn.predict(inputs)
+            print(f"Inputs: {inputs}, Expected: {expected_output}, Predicted: {prediction} = {round(prediction[0][0])}")
+
+    return 0
+
+
 
 def main(argv):
     args = Arguments()
@@ -9,7 +57,7 @@ def main(argv):
     elif args.help:
         return 0
 
-    return 0
+    return execute(args)
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
